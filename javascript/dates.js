96819347c5efd7619993a099db56ad138ee8ef25
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-02 09:00:01 +0530

    date.js included

---
+++ javascript/dates.js
@@ -0,0 +1,16 @@
+// We Set a TIME
+let a = new Date('2025-01-22')
+console.log(a)
+
+// let b = new Date(year,month,day,hours,mintutes,seconds,ms)
+let b = new Date(2018, 11, 24, 10, 33, 30, 0); 
+// Sept 2, 2025 at 14:30:15.250 (2:30:15 PM and 250 ms)
+console.log(b);
+
+
+let c = new Date('September 25, 2015 11:13:00');
+console.log(`Date String: `,c);
+
+const d = new Date("2022-03-25");
+console.log(`Date String: `,d);
+
