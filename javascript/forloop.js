41590a1d1257920686689b295524f110dc8028db
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-02 23:09:40 +0530

    $commit

--- javascript/forloop.js
+++ javascript/forloop.js
@@ -4,3 +4,9 @@ for(fetch=0; fetch < person_emp.length; fetch++){
      console.log(person_emp[fetch])
 }
 
+// fetch indexes
+const numberindex = new Array(1,2,3,4,5,6,7)
+for(circulate=1;circulate < numberindex.length;circulate++){
+      console.log(circulate)
+}
+
