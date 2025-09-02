2c5e9c9f6f141fa48c8077188a799b14ed6c051a
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 00:16:01 +0530

    $commit

--- javascript/scope.js
+++ javascript/scope.js
@@ -4,3 +4,19 @@ myfun()
 function myfun(){
     console.log(`I can Display ${store}`)
 }
+
+// Local Scope
+function greet(){
+    let message = `Local Scope: Hello World`
+    console.log(message)
+}
+
+greet()
+// console.log(message)
+
+
+{
+    let name1 = 'Alice'
+    console.log(name1)
+}
+// console.log(name1);
