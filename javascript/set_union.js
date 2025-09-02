a961f599d0af39af40a18cab79275440e2402b81
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 00:41:11 +0530

    $commit

--- javascript/set_union.js
+++ javascript/set_union.js
@@ -2,3 +2,7 @@ const a = new Set(['a','b','c'])
 const b = new Set(['b','c','d'])
 console.log(`Set Union`,a.union(b))
 
+
+const x = new Set(['a','b','c'])
+const y = new Set(['b','c','d'])
+console.log(`Set Union`,x.intersection(y))
