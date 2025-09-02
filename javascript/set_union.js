a3add2477098ce430f763bffad3b2c7e07a0eb56
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 01:09:31 +0530

    set union update

--- javascript/set_union.js
+++ javascript/set_union.js
@@ -5,4 +5,28 @@ console.log(`Set Union`,a.union(b))
 
 const x = new Set(['a','b','c'])
 const y = new Set(['b','c','d'])
-console.log(`Set Union`,x.intersection(y))
+console.log(`Set Intersection`,x.intersection(y))
+
+
+const c = new Set(['a','b','c'])
+const r = new Set(['b','c','d'])
+console.log(`Set Difference`,c.difference(r))
+
+
+const A = new Set(['a','b','c']);
+const B = new Set(['b','c','d']);
+
+const C = A.symmetricDifference(B);
+console.log(`Set SymmetricDifference`,C)
+
+
+const exe1 = new Set(['a','b','c']);
+const exe2 = new Set(['b','c','d']);
+let answer = exe1.isSupersetof(exe2);
+console.log(answer)
+
+
+const exe4 = new Set(['a','b','c']);
+const exe5 = new Set(['b','c','d']);
+let answer1 = exe4.isDisjointFrom(exe5);
+console.log(answer1)
