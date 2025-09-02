bfc364d9b2915b038216327482e49f1eb1ecb6b7
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-02 11:16:20 +0530

    $commit

--- javascript/if_else_loops.js
+++ javascript/if_else_loops.js
@@ -2,5 +2,18 @@ let age = 5
 if(age <=18){
     console.log("you are eligible");
 }else{
-    console.log('You are not  eligible');
+    console.log('You are not  eligible ','\n');
 }
+
+// if else if else:
+person = 'Rahul'
+
+if(person == 'Rahul'){
+       console.log(`Found Person in first level : ${person}`);
+}else if(person == 'Akash'){
+      console.log(`Found Person in Second Level: ${(person)}`)
+}else{
+    console.log(`Not Found in Any Level`);
+}
+
+
