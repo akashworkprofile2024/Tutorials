428eec028cce18524c798eab318051134555da58
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 09:42:29 +0530

    $commit

--- javascript/string_search.js
+++ javascript/string_search.js
@@ -3,4 +3,9 @@ console.log(`Index_OF: ${text.indexOf('locate')}`)
 console.log(`Index_OF: ${text.lastIndexOf('locate')}`)
 console.log(`string serach: ${text.search('locate')}`);
 console.log(`string serach: ${text.search(/locate/)}`);
+console.log(`Include: ${text.includes('locate')}`);
 
+let text1 = "Hello world, welcome to the universe.";
+let email = 'akash@gmail.com'
+console.log(`String Startswith: ${text1.startsWith('Hello')}`);
+console.log(`String Endswith: ${email.endsWith('@gmail.com')}`);
