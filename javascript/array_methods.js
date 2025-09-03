f673246a993c1f21c8d7d31f2a89b3cddebaf724
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 20:27:23 +0530

    update array method js file

--- javascript/array_methods.js
+++ javascript/array_methods.js
@@ -8,3 +8,13 @@ const arrvar2 = ['python','java' , 'C++']
 let mylist = arrvar2.toString();
 console.log(`Before Change: `, arrvar2 )
 console.log(`After Array to String: ${mylist}`)
+
+// Array at
+const arr3 = new Array('python' , 'java' , 'css')
+let storage = arr3.at(2)
+console.log(`Array At: ${storage}`)
+
+// Array Join
+const arr4 = new Array('python' , 'java' , 'css')
+let s3 = arr4.join('*')
+console.log(`Array Join: `,s3)
