77cb2bdf794f2214483bd513dd5e8800e74cb6d5
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 20:19:41 +0530

    update array method js file

---
+++ javascript/array_methods.js
@@ -0,0 +1,10 @@
+const arrvar = new Array('emp1','emp2','emp3','emp4')
+store = arrvar.length 
+console.log(`Array Length ${store}`)
+
+// Array to string
+ 
+const arrvar2 = ['python','java' , 'C++']
+let mylist = arrvar2.toString();
+console.log(`Before Change: `, arrvar2 )
+console.log(`After Array to String: ${mylist}`)
