e32a0eb59f43e8190b3f60442f87a81bb152ca9e
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-03 09:30:17 +0530

    $commit

--- javascript/string_search.js
+++ javascript/string_search.js
@@ -1,3 +1,6 @@
 let text = "Please locate where 'locate' occurs!"
 console.log(`Index_OF: ${text.indexOf('locate')}`)
 console.log(`Index_OF: ${text.lastIndexOf('locate')}`)
+console.log(`string serach: ${text.search('locate')}`);
+console.log(`string serach: ${text.search(/locate/)}`);
+
