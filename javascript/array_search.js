1b508ee2a939270359e1719b48bd85a05748ccdc
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-04 01:37:48 +0530

    include array search file

---
+++ javascript/array_search.js
@@ -0,0 +1,11 @@
+const person = new Array('emp1','emp2','emp3','emp4')
+console.log(`Array Index: ${person.indexOf('emp3') + 1}`);
+
+const person1 = new Array('emp1','emp2','emp3','emp2')
+console.log(`Array Last Index Of: ${person.lastIndexOf('emp2')+1}`);
+
+const person3 = new Array('Banana','Orange','Apple','Mango')
+console.log(`Array Includes: ${person3.includes('Mango')}`);
+
+
+
