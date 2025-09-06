64e644a9772da0bc87cb26ffde770c0f837bc18b
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-06 11:25:23 +0530

    chaiorcode -> conversion_operations.js

---
+++ javascript/chaiaurcode/conversion_operation.js
@@ -0,0 +1,18 @@
+let score = 33
+console.log(`33 is ${typeof(score)}`)
+let score1 = '55'
+console.log(`55 is ${typeof(score1)}`)
+
+let valein = Number(score1)
+console.log(`55 convert to ${typeof(score)}`)
+
+let valin = String(score)
+console.log(`33 convert ${typeof(valin)}`)
+
+
+let varlet = '33adb'
+let varletcon = Number(varlet)
+console.log(`33abc`,typeof varletcon)
+console.log(`33abc store as`,varletcon) // NAN
+
+
