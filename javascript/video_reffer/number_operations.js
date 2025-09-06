02c27e0cf3da115f52b0784f6b2143465147c685
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-06 12:43:11 +0530

    include video_reffer -> number_operations.js

--- javascript/video_reffer/number_operations.js
+++ javascript/video_reffer/number_operations.js
@@ -3,3 +3,28 @@ let negval = -val
 console.log(`Pos Val: ${val}`);
 console.log(`Neg Val: ${negval}`);
 
+let str1 = 'javascript'
+let str2 = 'Object Oriented Lang'
+console.log(`${str1 +' '+ str2}`); // Concate
+
+console.log(`concate num + string: ${1 + str2} , type after concate:  ${typeof( 1 + str2)}`);
+
+console.log(`str + num + num:  ${"1"+2+2} , Type : ${typeof ("1"+2+2)}`);   // recommanded
+console.log(`num+num+str: ${((1+2)+"2")} , Type: ${typeof(1+2+"2")}`); // not recommanded
+
+console.log(+true)   // not recommanded
+console.log(+'')     // note recommanded
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+console.log(`${num1 , num2 , num3}`);
+
+
+let gamecounter = 100
+gamecounter++    // +1 increment
+++gamecounter
+console.log(`Increment Gamecounter++: ${gamecounter}`);
+
+let x = 3;
+const y = x++;
+console.log(`${x} ${y}`);
