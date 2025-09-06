a16b8c6ae45b6cb72ba188e0d0ca3b4229341543
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-07 01:03:29 +0530

    update strings.js file

---
+++ javascript/video_reffer/strings.js
@@ -0,0 +1,18 @@
+const name = 'Akash'
+const repocount = 90
+console.log(`${name + repocount}`); // string interpolations
+
+const gamename = new String('Akash Biswas')
+console.log(`Gamename Var type: ${typeof gamename}`);
+
+console.log(`${gamename[0]}`);
+console.log(`Variable length: ${gamename.length}`);
+
+const stringvar = '    Hello World    '
+console.log(`${stringvar}`);
+console.log(`${stringvar.trim()}`);
+
+let namefile = 'The Whole World is blind'
+let store = namefile.split(',')
+console.log(`${store}`); 
+// console.log(`${namefile[0]}`);
