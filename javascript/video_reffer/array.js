16527095d6c596d4c7dab772a5050ec9cdf77378
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-08 10:40:46 +0530

    update array.js file

--- javascript/video_reffer/array.js
+++ javascript/video_reffer/array.js
@@ -1,3 +1,17 @@
 // array 
 
-const array = [1,2,3,4,5,6 , 'Fire' , True]
+const array = [1,2,3,4,5,6,'Fire' ,true]
+console.log(`Array: ${array[0]}`); // access value
+
+//  array always make swallo copy - > give same refference point , deep copy -> doesn,t give same refference
+
+const lang = new Array ('Python','Css','Html')
+console.log(`${lang}`); 
+
+// Array Methods
+console.log(`Push Element 10 and 15,16: ${array.push(10), array.push(15, 16) , array}`);
+console.log(`Array Length: ${lang.length}`);
+console.log(`Pop Element 16: ${array.pop() , array}`);
+
+array.unshift(9) // 9 add on starting of the array
+console.log(`${array}`);
