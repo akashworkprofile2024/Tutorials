06be452bef83c15d79abc859097ba0c8156dbe19
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-09 17:48:58 +0530

    update numpy arrays

--- python/advance_Python/numpy/create_numpy_arrays.py
+++ python/advance_Python/numpy/create_numpy_arrays.py
@@ -4,5 +4,10 @@ arr = akash.array([1,2,3,4,5])
 print(arr)
 print(type(arr))
 
-arr1 = ([1,2,3,4,5,6])
+arr1 = akash.array((1,2,3,4,5,6))
 print(arr1 , type(arr1))
+
+arr3 = ([1,2,3] , [4,5,6])
+print(arr3)
+
+
