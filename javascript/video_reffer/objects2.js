5485b692b29080567a955f81ff5e864ee15d6bee
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-10 00:52:03 +0530

    update objects2.js file

--- javascript/video_reffer/objects2.js
+++ javascript/video_reffer/objects2.js
@@ -1,9 +1,14 @@
 // // const firendsbook = new Object()
-// const firendsbook = {}
-// firendsbook.id = '1025ab'
-// firendsbook.name = 'Rahul'
-// firendsbook.islogin = false
-// // console.log(firendsbook);
+const firendsbook = {}
+firendsbook.id = '1025ab'
+firendsbook.name = 'Rahul'
+firendsbook.islogin = false
+console.log(firendsbook);
+
+console.log('fetch-keys of Objects: ',Object.keys(firendsbook));
+console.log('fetch-Values of Objects: ',Object.values(firendsbook));
+console.log('fetch-Entries of Objects: ',Object.entries(firendsbook))
+console.log(' Is firendsbook Own name Property: ', firendsbook.hasOwnProperty('name'))
 
 // const regular_user = {
 //     email:'rahul@gmail.com',
@@ -27,5 +32,15 @@
 const obj1 = {1:'a' , 2:'b'}
 const obj2 = {3:'c' , 4:'d'}
 // const obj3 = {obj1 , obj2}
-const obj3 = Object.assign({},obj1 , obj2)   // combine all elements in two different array
+// const obj4 = Object.assign({ },obj1 , obj2)   // combine all elements in two different array
+
+const obj3 = {...obj1,...obj2}
 console.log(obj3);
+
+const users=[
+    {
+        id: 1,
+        email:'a@gmail.com'
+    }
+]
+
