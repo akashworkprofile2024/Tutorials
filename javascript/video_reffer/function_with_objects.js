2c56657d694dabfcd10cb781281a742f6319d32a
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-10 14:36:32 +0530

    update function_with_objects.js file

---
+++ javascript/video_reffer/function_with_objects.js
@@ -0,0 +1,25 @@
+// Auto Parameter Pass 
+// function calculatecartprice(...num1){
+//           return num1;
+// }
+// console.log(calculatecartprice(200 , 400 , 500 , 2000),'\n');
+
+
+// function calculatecartprice2(val1 , val2 , ...num2){
+//       console.log(`Val1: `, val1);
+//       console.log(`Val2: `, val2);
+//       console.log(`Num2: `, num2);
+// }
+
+// console.log(calculatecartprice2(200 , 400 , 600 , 3000));
+
+const item={
+    item_name:'product1',
+    price:550
+}
+
+
+function handleobject(itempass){
+        console.log(`Item name is ${itempass.item_name} , and Price is:  ${itempass.price}`)
+}
+handleobject(item)
