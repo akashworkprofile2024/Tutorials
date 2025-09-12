0e5ef5a4c266927a31467f3aad1b6c67ab103030
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-12 13:37:25 +0530

    update control_flow.js file

---
+++ javascript/video_reffer/Advance/control_flow.js
@@ -0,0 +1,52 @@
+/*
+JavaScript में Control Flow क्या है?
+
+Control Flow का मतलब है, आपके प्रोग्राम के statements किस order में execute होंगे।
+यानि, किस तरह से आपका code एक लाइन से दूसरी लाइन पर जाएगा, और कौन सा हिस्सा कब चलेगा।
+
+Control Flow के मुख्य प्रकार:
+
+Sequential (क्रमिक):
+Statements एक के बाद एक चलते हैं।
+
+Conditional (शर्त आधारित):
+if, else, switch जैसे statements से code का flow condition के हिसाब से बदलता है।
+
+Loops (लूप्स):
+for, while, do...while जैसे loops से code बार-बार चलता है।
+*/ 
+
+// IF STATEMENT
+//condition should be true or false
+/*
+if(true){   
+    //  condition true block of code executed
+}
+
+if(false){
+    // condition false block of code not executed
+}
+
+Operators:
+< , > , <=, >= , ==(equal) , ===(Check Type Also)  , !=(Not Equal) , !== (negative Check Type Also)
+
+ */
+
+// const isuserloggedin=true;
+// const store = 'true'
+// if(isuserloggedin  === store){
+//     console.log('user logged in ')
+// }
+//   console.log(`Data Type Error: ${typeof store} `)
+
+
+// BLOCK SCOPE
+// const score = 400;
+// if(score > 100){
+//     let power = 'fly'
+//     console.log(`User Power:${power}`);
+// }
+//    console.log(`User Power:${power}`); // Power is not defined
+
+
+
