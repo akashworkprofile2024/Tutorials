dd4d4f0040cf725369a8a9c8f98abe5ee5233ee0
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-13 13:13:26 +0530

    update forloop_break_cont.js file

--- javascript/video_reffer/Advance/forloop_break_cont.js
+++ javascript/video_reffer/Advance/forloop_break_cont.js
@@ -6,7 +6,52 @@
 
 
 const arr = new Array('username1','username2','username3','username4','username5','username6','username7','username8','username9','username10');
-for(let x = 1 ; x <=arr.length ; x++){
-        const store = arr[x]
-        console.log(store);   
-} 
+
+// for(let x = 1 ; x <=arr.length ; x++){
+//        const store = arr[x]       // =>   Block Scope 
+//         console.log(store);        // =>    Block Scope
+                                                          
+// } 
+
+// console.log(store);   => Outside the For loop => Scope
+
+
+
+
+// for(let x  = 0 ; x <=5 ; x++){
+//     findnum = 3;
+//     if(x === findnum){
+//         console.log(`We Found: ${findnum}`);
+//         break;
+//     }else{
+//         console.log(`We Cant Found: ${findnum}`);
+//     }
+// }
+
+
+
+
+
+// for(let i = 0 ; i<=10 ; i++){
+//     console.log('\n')
+//     console.log(`Outter Loop: ${i}\n`);
+//     for(let j = 1 ; j<4 ; j++){
+//      console.log(`Inner Loop: ${j}`);
+//     }
+// }
+
+
+
+// let emp = ['Akash', 'Ramesh', 'Rahul', 'Priya', 'Anjali'];
+// let emp_dept = ['IT', 'HR', 'Finance', 'Marketing', 'Sales'];
+
+// for (let i = 0; i < emp.length; i++) {
+//     let emp_store = emp[i];
+//     for (let j = 0; j < emp_dept.length; j++) {
+//         let dept_store = emp_dept[j];
+//         console.log(`Employee_Name: ${emp_store} Department: ${dept_store}`);
+//     }
+// }
+
+
+
