16f1241561f42ceedfca211949157455460f3ac4
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-13 13:28:54 +0530

    update while_dowhile.js file

---
+++ javascript/video_reffer/Advance/while_dowhile.js
@@ -0,0 +1,18 @@
+// while(condition){
+
+// }
+
+// let index=0;
+// while (index<=10){
+//         console.log(`Value is : ${index}`);
+//         index = index +2; // increment by 2
+// }
+
+
+const myarray = new Array('Akash','Rahul','Ramesh','Priya');
+
+let index = 0;
+while(index <= myarray.length -1){
+     console.log(`Value are: ${myarray[index]}`);
+     index = index + 1  ; 
+}
