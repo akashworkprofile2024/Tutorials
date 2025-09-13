83ec37115ffcfbd732440c8449bff6a6ad8b7d14
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-14 01:12:29 +0530

    update highorder_Arrayloops.js file

---
+++ javascript/video_reffer/Advance/highorder_Arrayloops.js
@@ -0,0 +1,53 @@
+// For Loop
+
+// ["" , "" , ""] // Simple array
+
+// [{},{},{}] // Objects array
+
+// const arr = ["Python","Javascript","Java","C++","C"]
+
+// for(const num of arr){
+//      console.log(num , typeof num);
+// }
+
+// const greetings = "Hello World"
+// for(const greet of greetings){
+//     console.log(`Each Char is: ${greet}`);
+// }
+
+
+
+// MAPS 
+
+const arr = new Map();
+arr.set('IN','INDIA')
+arr.set('USA','UNITED STATES OF AMERICA')
+arr.set('FR','FRANCE')
+
+// console.log(arr);
+
+// for (const key of arr){
+//     console.log(key);
+// }
+
+// for(const [key,value] of arr){
+//       console.log(key , '>>', value);
+//     }
+
+
+
+
+
+
+
+const myobject ={
+    'game1': 'Nfs',
+    'game2':'fifa',
+    'game3':'pubg'
+}
+
+// for(const [key , value] of myobject ){     
+//      console.log(key , '>>' , value);
+// }
+
+
