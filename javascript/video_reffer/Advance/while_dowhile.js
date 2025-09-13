43bae62f23f2d7aecec4cec5568a056bf7e8cb95
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-13 13:35:35 +0530

    update while_dowhile.js file

--- javascript/video_reffer/Advance/while_dowhile.js
+++ javascript/video_reffer/Advance/while_dowhile.js
@@ -9,10 +9,39 @@
 // }
 
 
-const myarray = new Array('Akash','Rahul','Ramesh','Priya');
+// const myarray = new Array('Akash','Rahul','Ramesh','Priya');
 
-let index = 0;
-while(index <= myarray.length -1){
-     console.log(`Value are: ${myarray[index]}`);
-     index = index + 1  ; 
-}
+// let index = 0;
+// while(index <= myarray.length -1){
+//      console.log(`Value are: ${myarray[index]}`);
+//      index = index + 1  ; 
+// }
+
+
+// ============================= X ==================
+
+// DO   WHILE  LOOP
+// Do First then Check the While Condition 
+
+// do{
+
+// }while(condition){
+
+// }
+
+// let score = 1;    // Starting Point
+// do{
+//      console.log(`score: ${score}`);
+//      score = score + 1;
+// }
+// while(score <= 10){      // Condition The score should be less than or equal to 10
+// }
+
+
+// let score = 11;    // Starting Point
+// do{
+//      console.log(`score: ${score}`);
+//      score = score + 1;
+// }
+// while(score <= 10){      // Condition The score should be less than or equal to 10
+// }
