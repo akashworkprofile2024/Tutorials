c588fb0fc5f1e40820e0e5a7c2bc2587e5259f9a
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-13 12:07:38 +0530

    update forloop_break_cont.js file

---
+++ javascript/video_reffer/Advance/forloop_break_cont.js
@@ -0,0 +1,12 @@
+//  For LOOP
+
+// for(let i = 0  ; i <=10 ; i++){
+//     console.log(i);
+// }
+
+
+const arr = new Array('username1','username2','username3','username4','username5','username6','username7','username8','username9','username10');
+for(let x = 1 ; x <=arr.length ; x++){
+        const store = arr[x]
+        console.log(store);   
+} 
