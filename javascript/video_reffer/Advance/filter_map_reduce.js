0b72f30f2ffdea20a818f0f8c81bea59bcb8b2cd
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-14 23:55:33 +0530

    update filter_map_reduce.js file

---
+++ javascript/video_reffer/Advance/filter_map_reduce.js
@@ -0,0 +1,19 @@
+const coding = ['python','java','javascript']
+
+// const store = coding.forEach((item)=>{
+//         //  console.log(item);
+//          return item
+// })
+
+// console.log(`Values: ${store} >> Type: ${store}`);
+
+
+// FILTER OPERATIONS
+const num  = [1,2,3,4,5,6]
+// const store = num.filter((ele)=> ele > 2)
+// console.log(store)
+
+const store = num.filter((ele)=>{ele > 4})
+console.log(store)
+
+
