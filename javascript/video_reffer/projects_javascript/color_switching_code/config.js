d625f95417edb50eec875b2981cfc8f1164b46de
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-17 01:07:34 +0530

    include color_switcher.html -> color_switching_code

---
+++ javascript/video_reffer/projects_javascript/color_switching_code/config.js
@@ -0,0 +1,18 @@
+ const btn_store = document.querySelectorAll('.button')
+          const body = document.querySelector('body') 
+          btn_store.forEach(function(btn){
+            btn.addEventListener('click',function(e){
+                   if(e.target.id === 'pink'){
+                       body.style.backgroundColor=e.target.id
+                   }
+                    if(e.target.id === 'purple'){
+                       body.style.backgroundColor=e.target.id
+                   }
+                    if(e.target.id === 'yellow'){
+                       body.style.backgroundColor=e.target.id
+                   }
+                    if(e.target.id === 'orange'){
+                       body.style.backgroundColor=e.target.id
+                   }
+            });
+          });
