eed5518d1218d169ed8b16f208e826588eefd730
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-21 02:05:25 +0530

    TYPE OF FUNCTIONS

--- javascript/video_reffer/Advance/typesof_function.js
+++ javascript/video_reffer/Advance/typesof_function.js
@@ -1,4 +1,5 @@
 // ==============   SIMPLE FUNCTION  ===============
+// SYNTAX
 // function functionname(parameters){
 //        return value;
 // }
@@ -14,5 +15,39 @@
 
 // ==============   ASYNCHRONOUS FUNCTION  ===============
 
+// SYNTAX
+// async function functionname(parameters) {
+//       return block
+// }
+
+// async function fetchdata(){
+//         return "Data recieved";
+// }
+// fetchdata().then(result=>console.log(result))
 
 
+// async function addsync(a,b){
+//      return a + b ;
+// }
+// addsync(5 , 7).then(result=>console.log(`Addition: `,result))
+
+// ======================================================
+
+
+// ==============  ===   ARROW FUNCTION === =================
+
+// SYNTAX
+// const functionname = (parameters) =>{
+//     code block
+//     return something;
+// }
+
+// const multi = (a,b) => a*b;
+// console.log(`Multiple Result: `,multi(4,6));
+
+// const fetchuser = async()=>{
+//     return 'User Data';
+// }
+// fetchuser().then(data =>console.log(data))
+
+// ======================================================
