ebbf2fa1718fde55e3c182a3a76e835ac2099c43
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-21 00:03:21 +0530

    types of function

---
+++ javascript/video_reffer/Advance/typesof_function.js
@@ -0,0 +1,18 @@
+// ==============   SIMPLE FUNCTION  ===============
+// function functionname(parameters){
+//        return value;
+// }
+
+// function abc(a , b){
+//     return `Total: ${a + b}` ;
+// }
+// console.log(abc(5,3))
+// abc()
+
+// ==================================================
+
+
+// ==============   ASYNCHRONOUS FUNCTION  ===============
+
+
+
