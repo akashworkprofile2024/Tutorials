fb0d9c6124b885706ca8db85eafee34cb4b9d80d
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-22 02:40:31 +0530

    advance function

---
+++ javascript/advance/function.js
@@ -0,0 +1,32 @@
+// FUNCTION INVOCATION
+
+function number(a , b){
+       return  a + b
+}
+let store = number(2 ,3 )
+
+function number(a,b){
+    let store = a +b ;
+    console.log(store);
+}
+number(2,3)
+
+
+
+// ARROW FUNCTION
+
+arrfun = (num1 , num2) => num1 * num2;
+const storearrfun = (arrfun(2,3));
+console.log(storearrfun);
+
+
+
+
+// USING FUNCTION AS A VARIABLE
+
+let text = `The addition is:  ${e(5)}`
+console.log(text)
+function e(catchnum){
+      return catchnum + 5 ;   
+}
+
