f960b92ed5bd98a93e9d40698ad824e34e4bc900
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-23 00:29:54 +0530

    objects_defination

---
+++ javascript/advance/objects/objects_definations.js
@@ -0,0 +1,70 @@
+// BASIC OBJECTS
+// const userdetails = {name: 'Akash Biswas' , username: 'akash2000' , pass:'123'}
+// console.log(userdetails)
+// console.log(`Name: `, userdetails.name)
+// console.log(`Username: `, userdetails.username,'\n')
+
+
+
+// PUSH ELEMENT IN OBJECTS
+// const userdetails = {name: 'Akash Biswas' , username: 'akash2000' , pass:'123'}
+// userdetails.email = 'akash@gmail.com';
+// console.log(userdetails)
+
+
+// CREATE OBJECTS USING NEW KEYWORD
+// const userdetails = new Object({
+//         name: 'Akash Biswas',
+//         username:'akash2000',
+//         pass:'124',
+// })
+
+// console.log(userdetails)
+
+
+// JAVASCRIPT Object.create() 
+// const person={
+//     fname: 'Akash',
+//     lname:'biswas',
+// }
+// const man = Object.create(person)
+// console.log(`Before Change: `,person.fname)
+// man.fname = 'Rahul';
+// console.log(`After Change: `, man.fname)
+
+
+
+// JAVASCRIPT Object From Entiries
+// const course = [
+//     ['python' , 5000],
+//     ['C++' , 3000],
+//     ['Full Stack' , 25000]
+// ]
+// console.log(`Access Entiries: `,course[1])
+// console.log(`Total Length: ` , course.length)
+
+
+
+// JAVASCRIPT object.assign()
+// const userdetail={
+//                 fname:'Akash',
+//                 lname:'Biswas', 
+// }
+// const  accountdetail = {username: 'akash2000' , pass:'123'}
+// const combine = Object.assign(userdetail , accountdetail)
+// console.log(combine)
+// console.log(combine.username , combine.pass)
+
+
+// OBJECTS ARE MUTABLE
+// const person = {
+//     fname: 'Akash',
+//     lname:'Biswas'
+// }
+// // Try  to create a copy
+// const x = person;
+// //  Changes are easily occur
+// x.fname = 'Rahul';
+// console.log(x)
+
+
