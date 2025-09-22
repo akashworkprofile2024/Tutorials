4644b97992554187062a1da74c5bbf3265975306
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-23 01:43:35 +0530

    object method

---
+++ javascript/advance/objects/object_method.js
@@ -0,0 +1,47 @@
+// const person={
+//     fname:'Akash',
+//     lname:'Biswas',
+//     id:666777,
+//     fullname: function(){
+//             console.log(`Name:${person.fname} ID:${person.id}`);
+//             return `Name:${this.fname} ID:${this.id}`;   // this is person object
+            
+//     }
+// }
+// console.log(person.fullname())
+
+
+
+// ADDING A METHOD TO AN OBJECT
+// const person={
+//     fname:'John',
+//     lname:'Doe',
+//     id:666777,
+// };
+
+// // add a method
+// person.name=function(){
+//         return this.fname +' || '+ this.lname+' || '+this.id 
+// }
+// // Display Object Data
+// console.log(person.name())
+
+
+
+
+// USING JAVASCRIPT METHODS
+// const person={
+//     fname:'John',
+//     lname:'Doe',
+//     id:666777,
+// };
+
+// // add a method
+// person.name=function(){
+//         return this.fname.toUpperCase() +' || '+ this.lname.toUpperCase() +' || '+this.id 
+// }
+// // Display Object Data
+// console.log(person.name())
+
+
+
