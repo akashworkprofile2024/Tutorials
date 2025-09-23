42eac16ce3a619f8c7d48668ca28b252b5259cf1
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-23 10:03:34 +0530

    objects

---
+++ javascript/advance/objects/object_display.js
@@ -0,0 +1,34 @@
+// DISPLAY OBJECTS BY FOR LOOPS
+//     const userdetail={
+//         username:"akash2000",
+//         pass:"123",
+//         email:"akash@gmail.com",
+//     }
+
+// for (let x in userdetail){
+//        let store = userdetail[x]
+//        console.log(store , typeof store);       // userdetail.x   will not work  (Because x is the loop variable).
+// }
+
+
+
+// DISPLAY OBJECT VALUES AND KEY BOTH
+    // const userdetail={
+    //     username:"akash2000",
+    //     pass:"123",
+    //     email:"akash@gmail.com",
+    // }
+
+    // // create an array from the properties
+    // const keyarr = Object.keys(userdetail)
+    // const valuearr =Object.values(userdetail)
+
+    // for(let i= 0 ; i < keyarr.length;i++){
+    //        console.log(keyarr[i] ,":", valuearr[i])
+    // }
+
+
+
+
+    
+
