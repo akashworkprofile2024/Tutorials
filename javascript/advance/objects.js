2937f600fff389d91a2adaee3c7756f386bfeb25
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-23 09:33:10 +0530

    update

---
+++ javascript/advance/objects.js
@@ -0,0 +1,5 @@
+//Create Object literals   normal object
+const userdetails = {username:'Akash Biswas' , isloggin:true}
+console.log(userdetails)
+console.log(`fetch Username: `,userdetails.username)
+
