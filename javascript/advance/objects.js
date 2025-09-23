fce54f0c2bc55b15186aa99787717b4f2d51d394
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-09-23 10:03:22 +0530

    delete

--- javascript/advance/objects.js
+++
@@ -1,5 +0,0 @@
-//Create Object literals   normal object
-const userdetails = {username:'Akash Biswas' , isloggin:true}
-console.log(userdetails)
-console.log(`fetch Username: `,userdetails.username)
-
