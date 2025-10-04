77094aeb3c4ecfc3a58927bc235fed3f283805c5
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-10-04 17:12:45 +0530

    basics

--- javascript/excersie/basic/48.js
+++ javascript/excersie/basic/48.js
@@ -1,10 +1,13 @@
 const arr = [10,32,20];
- let store = 0;
-const variable = (recieve)=>{
-    for(let i = 0 ; i<recieve.length;i++){
+//  let store = 0;
+// const variable = (recieve)=>{
+//     for(let i = 0 ; i<recieve.length;i++){
        
-        store += recieve[i] ;
-    }
-    return store;
-};console.log(variable(arr))
+//         store += recieve[i] ;
+//     }
+//     return store;
+// };console.log(variable(arr))
 
+const sumarr = (recieve)=>
+    recieve.reduce((total , num)=>total + num , 0)
+console.log(sumarr(arr))
