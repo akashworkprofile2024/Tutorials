a9cbfef9e828c0f8794d009e05c14df97d78317f
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-10-08 12:05:49 +0530

    delete

--- javascript/vanilla_js_basics/date_set.js
+++
@@ -1,7 +0,0 @@
-const d = new Date('January 01, 2025')
-console.log(`Set Full Year: ${d.setFullYear(2020)}`);
-console.log(`Set Full Year: ${d.setFullYear(2020,11,3)}`);
-
-const dd = new Date();
-console.log(`Set Month: ${dd.setMonth(11)}`);
-console.log(`Set Date: ${dd.setDate(15)}`);
