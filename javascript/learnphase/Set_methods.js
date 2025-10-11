abd621d119df08d3278dc70c751fd08e619d72a1
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-10-11 10:41:16 +0530

    methods

--- javascript/learnphase/Set_methods.js
+++ javascript/learnphase/Set_methods.js
@@ -1,18 +1,19 @@
 // Create New Set
 const createarr = new Set([1,"Akash",3])
-console.log(createarr);
+console.log(`Set Created: `,createarr , typeof createarr,'\n');
 
 // Access Set Element from Set
 for(const x of createarr){
     console.log(x)
 }
+console.log('\n')
 
 // Create array from Set
 let store = [];
 for(let x of createarr){
     store.push(x)
 }
-console.log(`Set => Array:`,store.join(','))
+console.log(`Set => Array:`,store.join(','),`\n`)
 
 
 //Add new value in set
@@ -24,7 +25,39 @@ createarr1.forEach((e)=>{
     addele.add(e)   //   add element one by one
 })
 console.log(`Add element to Set: `, addele);
-console.log(`InstanceOf Set: `,addele instanceof Set)
+console.log(`InstanceOf Set: `,addele instanceof Set,'\n')
+
+// Set Size
+console.log(`Set Size:` , createarr.size,'\n')
+
+// Has Method
+const arr = [1,"python",'Java',2]
+let store1 = new Set()
+for(const xx of arr){
+    store1.add(xx)
+}
+
+if(store1.has('python')){
+    console.log(`Using Set Has: `,true,'\n');
+}else{
+    console.log(`Using Set Has: `,false,'\n')
+}
+
+
+// ForEach
+const createarr2 = new Set([22, 'Tanjiro' , 'Hugo' , '35'])
+createarr2.forEach((e)=>{
+    console.log(e  + "'s")
+})
+
+// Keys
+let keys_store = '';
+for(const  ee of createarr2.values()){
+    keys_store += ee
+}
+console.log(keys_store.split().slice().join(','))
+
+
 
 /*
 A JavaScript Set एक collection होता है unique values का।
@@ -32,4 +65,7 @@ A JavaScript Set एक collection होता है unique values का।
 हर value सिर्फ एक बार ही Set में आ सकती है।
 
 Values किसी भी प्रकार की हो सकती हैं — primitive values या objects।
+
+The has() method returns true if a specified value exists in a set.
+
 */
