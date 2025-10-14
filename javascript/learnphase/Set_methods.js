964d3d373e0865f312e0ced3b839b00997ed46fd
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-10-14 08:14:11 +0530

    methods

--- javascript/learnphase/Set_methods.js
+++ javascript/learnphase/Set_methods.js
@@ -2,11 +2,15 @@
 const createarr = new Set([1,"Akash",3])
 console.log(`Set Created: `,createarr , typeof createarr,'\n');
 
+//===========================================
+
 // Access Set Element from Set
 for(const x of createarr){
     console.log(x)
 }
 console.log('\n')
+
+//===========================================
 
 // Create array from Set
 let store = [];
@@ -15,6 +19,7 @@ for(let x of createarr){
 }
 console.log(`Set => Array:`,store.join(','),`\n`)
 
+//===========================================
 
 //Add new value in set
 
@@ -27,8 +32,12 @@ createarr1.forEach((e)=>{
 console.log(`Add element to Set: `, addele);
 console.log(`InstanceOf Set: `,addele instanceof Set,'\n')
 
+//===========================================
+
 // Set Size
 console.log(`Set Size:` , createarr.size,'\n')
+
+//===========================================
 
 // Has Method
 const arr = [1,"python",'Java',2]
@@ -37,12 +46,15 @@ for(const xx of arr){
     store1.add(xx)
 }
 
+//===========================================
+
 if(store1.has('python')){
     console.log(`Using Set Has: `,true,'\n');
 }else{
     console.log(`Using Set Has: `,false,'\n')
 }
 
+//===========================================
 
 // ForEach
 const createarr2 = new Set([22, 'Tanjiro' , 'Hugo' , '35'])
@@ -52,12 +64,16 @@ createarr2.forEach((e)=>{
 });
 console.log('\n')
 
+//===========================================
+
 // Keys
 let keys_store = '';
 for(const  ee of createarr2.values()){
     keys_store += ee + ","
 }
 console.log("Keys: " , keys_store.split().slice().join(','))
+
+//===========================================
 
 // Values
 const createarr3 = new Set(['Apple','Mango','Banana','Grapes','Apple'])
@@ -67,6 +83,8 @@ for(const ele of createarr3.values()){
 }
 console.log("Values", value_store,'\n')
 
+//===========================================
+
 // Entries
 const createarr3cpy = new Set(['Apple','Mango','Banana','Grapes','Apple'])
 let entries_store;
@@ -74,9 +92,58 @@ for(const enele of createarr3cpy.entries()){
     entries_store = enele;
 }
 console.log(`Entires` , createarr3cpy.entries(),'\n')
+//===========================================
+
+// SET LOGIC => UNION
+const a = new Set([1,2,3,4,3])
+const b = new Set([3,2,5,6,7,])
+let storeloginarr = Array.from(a.union(b))
+let converttoSet = new Set(storeloginarr)
+
+console.log(`Original Set A:`,a , b)
+console.log(`Union: `, storeloginarr.join(','));
+console.log(`Convert to Set: `, converttoSet , '\n');
+
+//================INTERSECTION===========================
+const x = new Set([1,2,3,4,3])
+const y = new Set([3,2,5,6,7,])
+let storeloginarr1 = Array.from(x.intersection(y))
+let converttoSet1 = new Set(storeloginarr1)
+
+console.log(`Original Set A:`,x , y)
+console.log(`Intersection: `, storeloginarr1.join(','));
+console.log(`Convert to Set: `, converttoSet1,'\n');
+
+//=================DIFFERENCE==========================
+const xx = new Set([1,2,3,4,3])
+const yy = new Set([3,2,5,6,7,])
+let storeloginarr2 = Array.from(a.difference(b))
+let converttoSet2 = new Set(storeloginarr2)
+
+console.log(`Original Set A:`,xx , yy)
+console.log(`Difference: `, storeloginarr2.join(','));
+console.log(`Convert to Set: `, converttoSet2);
+
+//===================SYMETTERIC DIFFERENCE========================
+const sym1 = new Set([1,2,3,4,3])
+const diff = new Set([3,2,5,6,7,])
+let storeloginarr3 = Array.from(symmetricDifference(sym1,diff))
+let converttoSet3 = new Set(storeloginarr3)
+
+console.log(`Original Set A:`,sym1 , diff)
+console.log(`Symettric Difference: `, storeloginarr3.join(','));
+console.log(`Convert to Set: `, converttoSet3);
 
 
+//=================isSubsetof==========================
+const z = new Set([1,2,3,4,3])
+const w = new Set([3,2,5,6,7,])
+let storeloginarr4 = Array.from()
+let converttoSet4 = new Set(z.issubsetOf(w))
 
+console.log(`Original Set A:`,z, w)
+console.log(`Symettric Difference: `, storeloginarr4.join(','));
+console.log(`Convert to Set: `, converttoSet4);
 /*
 A JavaScript Set एक collection होता है unique values का।
 
